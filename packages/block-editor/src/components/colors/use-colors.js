71ce2455f9d93d90468c0e287a7bed4f66240f1a
retrofox <rdsuarez@gmail.com>
2020-01-20 17:55:14 -0300

    use-colors: add attr for color value

--- packages/block-editor/src/components/colors/use-colors.js
+++ packages/block-editor/src/components/colors/use-colors.js
@@ -194,6 +194,7 @@ export default function __experimentalUseColors(
 						[ color ? name : camelCase( `custom ${ name }` ) ]: color ?
 							color.slug :
 							newColor,
+						[ camelCase( `value ${ name }` ) ]: newColor,
 					} );
 				},
 				{
