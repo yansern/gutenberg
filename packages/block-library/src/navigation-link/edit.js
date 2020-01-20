969c73d3bf14a8d7573ec2f6407d726400b0ba77
retrofox <rdsuarez@gmail.com>
2020-01-20 09:48:47 -0300

    navigation-link: add border-color inline style

--- packages/block-library/src/navigation-link/edit.js
+++ packages/block-library/src/navigation-link/edit.js
@@ -48,6 +48,7 @@ function NavigationLinkEdit( {
 	setAttributes,
 	showSubmenuIcon,
 	insertLinkBlock,
+	navigationAttributes,
 } ) {
 	const { label, opensInNewTab, title, url, nofollow, description } = attributes;
 	const link = {
@@ -147,13 +148,15 @@ function NavigationLinkEdit( {
 					/>
 				</PanelBody>
 			</InspectorControls>
-			<div className={ classnames(
-				'wp-block-navigation-link', {
-					'is-editing': isSelected || isParentOfSelectedBlock,
-					'is-selected': isSelected,
-					'has-link': !! url,
-					'has-child': hasDescendants,
-				} ) }
+			<div
+				className={ classnames(
+					'wp-block-navigation-link', {
+						'is-editing': isSelected || isParentOfSelectedBlock,
+						'is-selected': isSelected,
+						'has-link': !! url,
+						'has-child': hasDescendants,
+					} ) }
+				style={ navigationAttributes.customTextColor ? { borderColor: navigationAttributes.customTextColor } : null }
 			>
 				<div className="wp-block-navigation-link__content">
 					<RichText
