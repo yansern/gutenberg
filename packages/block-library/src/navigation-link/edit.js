3b2389a3bd8ffd29f8e76510c391790453259d08
retrofox <rdsuarez@gmail.com>
2020-01-21 18:57:43 -0300

    navigation-item: use detected color as default

--- packages/block-library/src/navigation-link/edit.js
+++ packages/block-library/src/navigation-link/edit.js
@@ -161,8 +161,8 @@ function NavigationLinkEdit( {
 						[ `has-${ navigationBlockAttributes.backgroundColor }-background-color` ]: !! navigationBlockAttributes.backgroundColor,
 					} ) }
 				style={ {
-					borderColor: navigationBlockAttributes.valueTextColor,
-					color: navigationBlockAttributes.valueTextColor,
+					// borderColor: navigationBlockAttributes.valueTextColor,
+					color: navigationBlockAttributes.detectedTextColor,
 					backgroundColor: navigationBlockAttributes.valueBackgroundColor,
 				} }
 			>
