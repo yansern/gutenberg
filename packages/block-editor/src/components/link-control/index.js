7a87eab9db7fbf8c91d117f205d5c950ad65a173
Dave Smith <getdavemail@gmail.com>
2020-01-23 12:53:14 +0000

    Generalise prop to enable on the fly creation of entities

--- packages/block-editor/src/components/link-control/index.js
+++ packages/block-editor/src/components/link-control/index.js
@@ -35,7 +35,7 @@ function LinkControl( {
 	settings,
 	onChange = noop,
 	showInitialSuggestions,
-	showCreatePages,
+	showCreateEntity,
 	createEmptyPage,
 } ) {
 	const instanceId = useInstanceId( LinkControl );
@@ -134,7 +134,7 @@ function LinkControl( {
 
 		const directLinkEntryTypes = [ 'url', 'mailto', 'tel', 'internal' ];
 		const isSingleDirectEntryResult = suggestions.length === 1 && directLinkEntryTypes.includes( suggestions[ 0 ].type.toLowerCase() );
-		const shouldShowCreateEntity = showCreatePages && createEmptyPage && ! isSingleDirectEntryResult;
+		const shouldShowCreateEntity = showCreateEntity && createEmptyPage && ! isSingleDirectEntryResult;
 
 		// According to guidelines aria-label should be added if the label
 		// itself is not visible.
