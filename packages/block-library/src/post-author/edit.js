fb67e5cc9f618ede9311abafd5ccc6d25716f843
Andrei Draganescu <andrei.draganescu@automattic.com>
2020-01-27 13:22:52 +0200

    adds color controls to post author

--- packages/block-library/src/post-author/edit.js
+++ packages/block-library/src/post-author/edit.js
@@ -1,20 +1,46 @@
 /**
  * WordPress dependencies
  */
+import { useRef } from '@wordpress/element';
 import { useEntityProp } from '@wordpress/core-data';
-import { AlignmentToolbar, BlockControls, RichText } from '@wordpress/block-editor';
+import {
+	AlignmentToolbar,
+	BlockControls,
+	RichText,
+	__experimentalUseColors,
+	withFontSizes,
+} from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
 
-export default function PostAuthorEdit( { setAttributes } ) {
-	const [ authorId ] = useEntityProp( 'postType', 'post', 'author' );
-	const author = useSelect(
-		( select ) => select( 'core' ).getEntityRecord( 'root', 'user', authorId ),
-		[ authorId ]
-	);
+/**
+ * Internal dependencies
+ */
+import BlockColorsStyleSelector from './block-colors-selector';
 
-	if ( ! author ) {
-		return false;
-	}
+function PostAuthorDisplay( { props, author } ) {
+	const ref = useRef();
+
+	const { setAttributes, fontSize } = props;
+
+	const {
+		TextColor,
+		BackgroundColor,
+		InspectorControlsColorPanel,
+		ColorPanel,
+	} = __experimentalUseColors(
+		[
+			{ name: 'textColor', property: 'color' },
+			{ name: 'backgroundColor', className: 'background-color' },
+		],
+		{
+			contrastCheckers: [ { backgroundColor: true, textColor: true, fontSize: fontSize.size } ],
+			colorDetector: { targetRef: ref },
+			colorPanelProps: {
+				initialOpen: true,
+			},
+		},
+		[ fontSize.size ]
+	);
 
 	setAttributes( {
 		author,
@@ -24,8 +50,19 @@ export default function PostAuthorEdit( { setAttributes } ) {
 		<>
 			<BlockControls>
 				<AlignmentToolbar />
+				<BlockColorsStyleSelector
+					TextColor={ TextColor }
+					BackgroundColor={ BackgroundColor }
+				>
+
+					{ ColorPanel }
+
+				</BlockColorsStyleSelector>
 			</BlockControls>
-			<div className="wp-block-post-author">
+
+			{ InspectorControlsColorPanel }
+
+			<div ref={ ref } className="wp-block-post-author">
 				<img src={ author.avatar_urls[ 24 ] } alt={ author.name } className="wp-block-post-author__avatar" />
 				<RichText
 					className="wp-block-post-author__name"
@@ -36,3 +73,19 @@ export default function PostAuthorEdit( { setAttributes } ) {
 		</>
 	);
 }
+
+function PostAuthorEdit( props ) {
+	const [ authorId ] = useEntityProp( 'postType', 'post', 'author' );
+	const author = useSelect(
+		( select ) => select( 'core' ).getEntityRecord( 'root', 'user', authorId ),
+		[ authorId ]
+	);
+
+	if ( ! author ) {
+		return 'Post Author Placeholder';
+	}
+
+	return <PostAuthorDisplay props={ props } author={ author } />;
+}
+
+export default withFontSizes( 'fontSize' )( PostAuthorEdit );
